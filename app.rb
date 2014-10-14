64bae8c63c22e7c4729ce046bd10b17005925d47
kevinbleich <kevin@wintr.us>
2014-10-13 20:02:36 -0700

    debug heroku

--- app.rb
+++ app.rb
@@ -122,10 +122,11 @@ get '/auth/:provider/callback' do
   @title = 'Wikiedu Wizard - OAuth'
   @auth = request.env['omniauth.auth']
   @access_token = request.env["omniauth.auth"]["extra"]["access_token"]
-  @access_token.inspect
-  # session['access_token'] = @access_token.token
-  # session['access_token_secret'] = @access_token.secret
+  # @access_token.inspect
+  session['access_token'] = @access_token.token
+  session['access_token_secret'] = @access_token.secret
   
-  redirect to '/client'
+  request.env.inspect
+  # redirect to '/client'
 
 end
