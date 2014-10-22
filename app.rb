36cae9d6db3ffde6d973ce7391fb91679ab638a7
kevinbleich <kevin@wintr.us>
2014-10-22 12:03:48 -0700

    e

--- app.rb
+++ app.rb
@@ -71,19 +71,15 @@ post '/publish' do
   @wizardData = params['wikitext']
 
   # PROBABLY SHOULD BE BROKEN OUT INTO A SEPERATE FUNCTION
-  conn = OAuth::Consumer.new(ENV["WIKI_KEY"], ENV["WIKI_SECRET"])
-
-  access_token = OAuth::AccessToken.new(conn, session['access_token'], session['access_token_secret'])
-
-  get_token = access_token.get('https://en.wikipedia.org/w/api.php?action=query&meta=tokens&format=json')
-
+  @conn = OAuth::Consumer.new(ENV["WIKI_KEY"], ENV["WIKI_SECRET"])
+  @access_token = OAuth::AccessToken.new(@conn, session['access_token'], session['access_token_secret'])
+  get_token = @access_token.get('https://en.wikipedia.org/w/api.php?action=query&meta=tokens&format=json')
   token_response = JSON.parse(get_token.body)
-
   csrf_token = token_response['query']['tokens']['csrftoken']
   
-  res = access_token.post('http://en.wikipedia.org/w/api.php', {:action => 'edit', :title => "User:Dmak78/Course Testing 2", :text => '', :format => 'json', :token => csrf_token } )
+  res = @access_token.post('http://en.wikipedia.org/w/api.php', {:action => 'edit', :title => "User:#{session['wiki_username']}/#{params['course_title']}", :text => @wizardData, :format => 'json', :token => csrf_token } )
 
-  return csrf_token
+  return res.body
 end
 
 
@@ -91,13 +87,9 @@ get '/client' do
 
   # PROBABLY SHOULD BE BROKEN OUT INTO A SEPERATE FUNCTION
   @conn = OAuth::Consumer.new(ENV["WIKI_KEY"], ENV["WIKI_SECRET"])
-
   @access_token = OAuth::AccessToken.new(@conn, session['access_token'], session['access_token_secret'])
-
   get_token = @access_token.get('https://en.wikipedia.org/w/api.php?action=query&meta=tokens&format=json')
-
   token_response = JSON.parse(get_token.body)
-
   csrf_token = token_response['query']['tokens']['csrftoken']
   
   res = @access_token.post('http://en.wikipedia.org/w/api.php', {:action => 'query', :meta => 'userinfo', :format => 'json' } )
